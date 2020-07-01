34f276c13ebd2e73a095a27d11ab98d2f0aefd63
filodoks <filodoks@gmail.com>
2020-07-01 09:29:40 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+// CHAPTER 2
+
+import (
+	"fmt"
+	"monkey/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programing language!\n",
+		user.Username)
+		fmt.Printf("Feel free to type in commands\n")
+		repl.Start(os.Stdin, os.Stdout)
+}
